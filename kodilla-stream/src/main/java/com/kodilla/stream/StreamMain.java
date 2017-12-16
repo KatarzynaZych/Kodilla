bae44dd5673cefbf6f521835b8b3c59b9737ab1a
KatarzynaZych <korzonek.katarzyna@gmail.com>
2017-12-16 01:47:07 +0000

    Zadanie 7.1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,18 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("word",text -> "ABC" + text + "ABC");
+        poemBeautifier.beautify("word",text ->  text +"验版");
+        poemBeautifier.beautify("word", text -> '♠' + text);
+        poemBeautifier.beautify("word",String::toUpperCase);
+        poemBeautifier.beautify("WORD",String::toLowerCase);
+
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/lambda/Processor.java
@@ -0,0 +1,9 @@
+package com.kodilla.stream.lambda;
+
+public class Processor {
+
+    public void excute(Executor executor) {
+
+        executor.process();
+    }
+}
