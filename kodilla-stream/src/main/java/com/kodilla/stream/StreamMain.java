42da57a7e1d7e1ddd16e008464c5555ea2cade11
KatarzynaZych <korzonek.katarzyna@gmail.com>
2017-12-17 17:22:53 +0000

    Skrypt z modulu 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,24 +1,23 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.*;
-import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
+
     public static void main(String[] args) {
 
-//        PoemBeautifier poemBeautifier = new PoemBeautifier();
-//        poemBeautifier.beautify("word",text -> "ABC" + text + "ABC");
-//        poemBeautifier.beautify("word",text ->  text +"验版");
-//        poemBeautifier.beautify("word", text -> '♠' + text);
-//        poemBeautifier.beautify("word",String::toUpperCase);
-//        poemBeautifier.beautify("WORD",String::toLowerCase);
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));
 
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        System.out.println(theResultStringOfBooks);
     }
-
-
 
 }
