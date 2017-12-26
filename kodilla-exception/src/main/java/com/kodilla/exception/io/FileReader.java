d594f8e10975963b402f6fc8c301c62a8ea1af17
KatarzynaZych <korzonek.katarzyna@gmail.com>
2017-12-26 22:07:21 +0000

    skrypt z rozdzialu 8.1

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -0,0 +1,15 @@
+package com.kodilla.exception.io;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class FileReader {
+
+    public void readFile() throws IOException {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+    }
+
+}
