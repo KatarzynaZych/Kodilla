9ccc08f8189d55348041c158ad59010260873031
KatarzynaZych <korzonek.katarzyna@gmail.com>
2017-12-28 16:39:49 +0000

    rozdzial 8.1

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -3,6 +3,7 @@ package com.kodilla.exception.io;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReader {
@@ -10,6 +11,17 @@ public class FileReader {
     public void readFile() throws IOException {
 
         ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResources("file/names.txt").getFile());
+        Path path = Paths.get(file.getPath());
+
+        try {
+            Stream<String> fileLines = Files.lines(path);
+        }catch (IOException e){
+            System.out.println("Oh no! oh oh ");
+        }
+
+
+        System.out.println(file.getPath());
     }
 
 }
