c6e44bd6be1bc943c6bd06b066bf555cb53e8670
KatarzynaZych <korzonek.katarzyna@gmail.comgit config --global user.name KatarzynaZych>
2018-04-07 00:31:49 +0100

    zadanie 20.3

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
@@ -2,18 +2,20 @@ package com.kodilla.patterns2.facade.api;
 
 import com.kodilla.patterns2.facade.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Service;
 import org.slf4j.*;
 
 import java.math.BigDecimal;
 
 @Service
+@EnableAspectJAutoProxy
 public class OrderFacade {
     @Autowired
     private ShopService shopService;
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
 
-    public void processOrder(final OrderDto orderDto, final Long userId) throws OrderProcessingException {
+    public void processOrder(final OrderDto orderDto,final Long userId) throws OrderProcessingException {
         boolean wasError = false;
         long orderId = shopService.openOrder(userId);
         LOGGER.info("Registering new order, ID: " + orderId);
